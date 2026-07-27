ba46ce5fe279cd093873ddf61c56ef55bff2fa17
shahadkhraisheh <153379919+shahadkhraisheh@users.noreply.github.com>
2026-07-27 21:55:40 +0300

    add the routes

--- event-booking-system/src/app/app.routes.ts
+++ event-booking-system/src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
+import { authGuardGuard } from './core/guard/auth.guard-guard';
 
 export const routes: Routes = [
-
-
+{path: '', loadComponent: () => import('./feature/events/event-list/event-list').then(m => m.EventList)},
+{path:'login', loadComponent: () => import('./feature/users/login/login').then(m => m.Login)},
+{path:'signup', loadComponent: () => import('./feature/users/signup/signup').then(m => m.Signup)},
+{path:'bookings', loadComponent: () => import('./feature/booking/booking-list/booking-list').then(m => m.BookingList), canActivate: [authGuardGuard]},
 
 ];
