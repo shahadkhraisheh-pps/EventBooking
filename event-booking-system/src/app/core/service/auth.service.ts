28f7183780437c064dab46536c6dbd8c31f215a4
shahadkhraisheh <153379919+shahadkhraisheh@users.noreply.github.com>
2026-07-27 22:00:37 +0300

    add the auth service

--- event-booking-system/src/app/core/service/auth.service.ts
+++ event-booking-system/src/app/core/service/auth.service.ts
@@ -1,8 +1,45 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Auth, AuthProvider, authState, createUserWithEmailAndPassword, GoogleAuthProvider, signInWithEmailAndPassword, signInWithRedirect, signOut, User } from '@angular/fire/auth';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+   // Inject the modern Auth instance and HttpClient using the inject() function
+  private auth = inject(Auth);
+  private http = inject(HttpClient);
+
+  constructor() { }
   
+  getCurrentUser(): Observable<User | null> {
+    return authState(this.auth);
+  }
+
+  signUpWithEmailAndPassword(email: string, password: string): Promise<any> {
+    return createUserWithEmailAndPassword(this.auth, email, password);
+  }
+
+  loginWithEmailAndPassword(email: string, password: string): Promise<any> {
+    return signInWithEmailAndPassword(this.auth, email, password);
+  }
+
+  signInWithProvider(provider: AuthProvider): Promise<void> {
+    return signInWithRedirect(this.auth, provider);
+  }
+
+  signInWithGoogle(): Promise<void> {
+    const provider = new GoogleAuthProvider();
+    return this.signInWithProvider(provider);
+  }
+
+  loginWithGoogle(): Promise<void> {
+    const provider = new GoogleAuthProvider();
+    return this.signInWithProvider(provider);
+  }
+
+  logout(): Promise<void> {
+    return signOut(this.auth);
+  }
 }
