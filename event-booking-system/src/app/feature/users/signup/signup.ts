e28f5b3ac26292cc621ac6a1dff5e8674d6ae688
shahadkhraisheh <153379919+shahadkhraisheh@users.noreply.github.com>
2026-07-27 22:11:59 +0300

    add the sign up component

--- event-booking-system/src/app/feature/users/signup/signup.ts
+++ event-booking-system/src/app/feature/users/signup/signup.ts
@@ -1,11 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../../core/service/auth.service';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [ReactiveFormsModule,RouterLink],
   templateUrl: './signup.html',
   styleUrl: './signup.css',
 })
 export class Signup {
+isLoginMode=true;
+isLoading=signal(false);
+error:string='';
+constructor(private authService: AuthService,private router:Router) { }
+signupForm:FormGroup=new FormGroup({
+     email: new FormControl('', [
+      Validators.required,
+      Validators.email,
+      Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+      Validators.pattern(/[0-9a-zA-Z]{6,}/)
+    ])})
 
+ngOnInit(): void {
+}
+
+signUpWithEmailAndPassword(){
+  const email=this.signupForm.value?.email;
+  const password=this.signupForm.value?.password;
+  this.authService.signUpWithEmailAndPassword(email,password).then(
+    (user) => {
+      this.router.navigate(['/events']);
+    }
+  ).catch((error) => {
+    alert('Error signing up:'+error);
+    this.error = 'An error occurred during signup. Please try again.';
+  });
+}
+  signUpWithGoogle():void{
+    this.authService.signInWithGoogle().then(
+      (user) => {
+        this.router.navigate(['/events']);
+      }
+    ).catch((error) => {
+      alert('Error signing up with Google:'+error);
+      this.error = 'An error occurred during signup with Google. Please try again.';
+    });
+  }
 }
