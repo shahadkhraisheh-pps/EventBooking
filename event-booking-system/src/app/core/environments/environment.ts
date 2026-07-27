f4df7bff28fa569775003b42a373f858e2bee876
shahadkhraisheh <153379919+shahadkhraisheh@users.noreply.github.com>
2026-07-27 21:24:12 +0300

    create the firebase enviroment

---
+++ event-booking-system/src/app/core/environments/environment.ts
@@ -0,0 +1,11 @@
+
+export const firebaseConfig = {
+ apiKey: "AIzaSyBQtAS_OJynh4n3vRbf5DYr5ELuGLwI2rY",
+  authDomain: "eventbooking-7cb94.firebaseapp.com",
+  databaseURL: "https://eventbooking-7cb94-default-rtdb.firebaseio.com",
+  projectId: "eventbooking-7cb94",
+  storageBucket: "eventbooking-7cb94.firebasestorage.app",
+  messagingSenderId: "136835200955",
+  appId: "1:136835200955:web:080d1edf9483d9a84ff69e",
+  measurementId: "G-8HMR06QL0B"
+};
