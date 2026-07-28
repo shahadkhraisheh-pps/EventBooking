24bc805f997017e615544612f51a03b2716e7ccd
shahadkhraisheh <153379919+shahadkhraisheh@users.noreply.github.com>
2026-07-28 11:59:09 +0300

    edit the header

--- event-booking-system/src/app/shared/component/header/header.ts
+++ event-booking-system/src/app/shared/component/header/header.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from "@angular/router";
+import { AuthService } from '../../../core/service/auth.service';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
+ private authService=inject(AuthService);
 
+
+    isLoggedIn = this.authService.isLoggedIn;
+
+
+  logout() {
+    this.authService.logout();
+  }
 }
