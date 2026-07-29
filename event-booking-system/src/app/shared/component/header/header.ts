f7e6d5940772ee2c47ff275a2b0733029c733700
shahadkhraisheh <153379919+shahadkhraisheh@users.noreply.github.com>
2026-07-29 16:59:49 +0300

    edit the header style and add user info

--- event-booking-system/src/app/shared/component/header/header.ts
+++ event-booking-system/src/app/shared/component/header/header.ts
@@ -1,16 +1,19 @@
 import { Component, inject } from '@angular/core';
 import { Router, RouterLink } from "@angular/router";
 import { AuthService } from '../../../core/service/auth.service';
+import {MatIconModule} from '@angular/material/icon'
+
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterLink,MatIconModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
  private authService=inject(AuthService);
- private route=inject(Router)
+ private route=inject(Router);
+ currentUser=this.authService.currentUser;
 
   isLoggedIn = this.authService.isLoggedIn;
 
