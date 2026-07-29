24ec06b3d2e60be9b17e5cf5712315adfbdecf7c
shahadkhraisheh <153379919+shahadkhraisheh@users.noreply.github.com>
2026-07-29 11:04:06 +0300

    add the loading spinner

--- event-booking-system/src/app/feature/users/signup/signup.ts
+++ event-booking-system/src/app/feature/users/signup/signup.ts
@@ -2,18 +2,22 @@ import { Component, signal } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../../core/service/auth.service';
+import { LoadingSpinner } from "../../../shared/loading-spinner/loading-spinner";
 
 @Component({
   selector: 'app-signup',
-  imports: [ReactiveFormsModule,RouterLink],
+  imports: [ReactiveFormsModule, RouterLink, LoadingSpinner],
   templateUrl: './signup.html',
   styleUrl: './signup.css',
 })
 export class Signup {
-isLoginMode=true;
+//for the loading spinner
 isLoading=signal(false);
+//for the error massage
 error:string='';
+
 constructor(private authService: AuthService,private router:Router) { }
+//signup form
 signupForm:FormGroup=new FormGroup({
   username: new FormControl('', [
     Validators.required,
@@ -33,28 +37,45 @@ signupForm:FormGroup=new FormGroup({
 
 ngOnInit(): void {
 }
-
+//signup method that with email and password form the auth service 
 signUpWithEmailAndPassword(){
+  
+  //take the data form the signup input
   const email=this.signupForm.value?.email;
   const password=this.signupForm.value?.password;
   const username=this.signupForm.value?.username;
+
+ //while login process work the loading spinner      
+  this.isLoading.set(true);
+//call the method from the service
   this.authService.signUpWithEmailAndPassword(username, email, password).then(
     (user) => {
-      this.router.navigate(['/events']);
+      this.router.navigate(['/events']);  //if all good take you the event page 
+    this.isLoading.set(false);
+      
     }
   ).catch((error) => {
     alert('Error signing up:'+error);
-    this.error = 'An error occurred during signup. Please try again.';
+    this.error = 'An error occurred during signup. Please try again.';//if not show error massage
+     this.isLoading.set(true);
+
   });
 }
+//signup with google provider
   signUpWithGoogle():void{
+        this.isLoading.set(false);
+
     this.authService.loginWithGoogle().then(
       (user) => {
         this.router.navigate(['/events']);
+         this.isLoading.set(true);
+
       }
     ).catch((error) => {
       alert('Error signing up with Google:'+error);
       this.error = 'An error occurred during signup with Google. Please try again.';
+          this.isLoading.set(true);
+
     });
   }
 }
