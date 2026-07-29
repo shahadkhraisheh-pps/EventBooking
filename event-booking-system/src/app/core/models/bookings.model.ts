ae69115db3b364336c7b297cd9ff0c96d8e6a6bd
shahadkhraisheh <153379919+shahadkhraisheh@users.noreply.github.com>
2026-07-29 19:34:28 +0300

    add  seate quntity feild

--- event-booking-system/src/app/core/models/bookings.model.ts
+++ event-booking-system/src/app/core/models/bookings.model.ts
@@ -1,7 +1,14 @@
+import { Events } from "./events.model";
+
 export interface Bookings {
   id: string;
   eventId: string;
   userId: string;
   bookingDate: string;
-  status: string;
+  status:  'confirmed' | 'cancelled';
+  seatsqty:number;
+}
+export interface BookingAndEvent{
+  booking:Bookings;
+  event:Events |undefined;
 }
