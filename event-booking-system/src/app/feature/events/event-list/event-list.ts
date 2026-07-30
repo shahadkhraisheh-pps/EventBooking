95810a7e2b4fae5b1259ad2c4e03c5ffd64d4980
shahadkhraisheh <153379919+shahadkhraisheh@users.noreply.github.com>
2026-07-30 10:54:59 +0300

    add the filter section on event list

--- event-booking-system/src/app/feature/events/event-list/event-list.ts
+++ event-booking-system/src/app/feature/events/event-list/event-list.ts
@@ -1,56 +1,96 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
 import { Events } from '../../../core/models/events.model';
 import { EventsService } from '../../../core/service/events.service';
 import { Router, RouterLink } from '@angular/router';
-import { AsyncPipe, DatePipe } from '@angular/common';
+import { AsyncPipe, CommonModule, DatePipe } from '@angular/common';
 import { BookingsService } from '../../../core/service/bookings.service';
 import { AuthService } from '../../../core/service/auth.service';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
-import { LoadingSpinner } from "../../../shared/loading-spinner/loading-spinner";
+import { LoadingSpinner } from '../../../shared/loading-spinner/loading-spinner';
 
 @Component({
   selector: 'app-event-list',
-  imports: [AsyncPipe, DatePipe, ReactiveFormsModule, MatIconModule, LoadingSpinner],
+  imports: [
+    AsyncPipe,
+    DatePipe,
+    ReactiveFormsModule,
+    MatIconModule,
+    LoadingSpinner,
+    CommonModule,
+FormsModule  ],
   templateUrl: './event-list.html',
   styleUrl: './event-list.css',
 })
-export class EventList implements OnInit{
-    isLoading=signal(false);
-private eventService=inject(EventsService);
-private route=inject(Router);
-private bookingService=inject(BookingsService);
-private authService=inject(AuthService);
-
-//add the input form for seats quntity
-bookForm: FormGroup = new FormGroup({
-    quntity: new FormControl('', [Validators.required]),    
+export class EventList implements OnInit {
+  isLoading = signal(false);
+  private eventService = inject(EventsService);
+  private route = inject(Router);
+  private bookingService = inject(BookingsService);
+  private authService = inject(AuthService);
+  searchByName = signal<string>('');
+  searchByDate = signal<string>('');
+  searchByLocation = signal<string>('');
+  bookedSeates = signal<number>(1000);
+  events = signal<Events[]>([]);
+  //add the input form for seats quntity
+  bookForm: FormGroup = new FormGroup({
+    quntity: new FormControl('', [Validators.required]),
   });
 
   //get the event data form the srvice to show in the page
   events$!: Observable<Events[]>;
-ngOnInit(){
-this.events$=this.eventService.getEvents();
-}
-
-//book the event by event id and the seats quntity
-bookEvent(eventId:string){
-const formValue=this.bookForm.value;
-  this.isLoading.set(true)
-  if(!this.authService.isLoggedIn()){
-    this.route.navigate(['/login']);
-    return
+  ngOnInit() {
+    this.events$ = this.eventService.getEvents();
+    this.events$.subscribe((events) => this.events.set(events));
   }
 
-  this.bookingService.bookEvent(eventId,formValue.quntity) //use the method in booking service
-  .then(()=>{this.route.navigate(['/bookings'])
-    this.isLoading.set(false);
+  fillteredEvent = computed(() => {
+    let result=[...this.events()];
 
+     // Filter by Name
+    if (this.searchByName().trim()) {//take the name from the ngmodel and format it and searche in the event title 
+      const query = this.searchByName().toLowerCase().trim();
+      result = result.filter(e => e.title.toLowerCase().includes(query));
+    }
+
+    //Filter by Date 
+    if(this.searchByDate()){
+      result=result.filter(e=>e.date === this.searchByDate() )
+    }
+
+    //Filter by Location
+    if(this.searchByLocation()){
+      result=result.filter(e=>e.location===this.searchByLocation())
+    }
+
+    if(this.bookedSeates()){
+      result=result.filter(e=>e.capacity>=this.bookedSeates())
+    }
+
+
+    return result;
+  });
+
+  //book the event by event id and the seats quntity
+  bookEvent(eventId: string) {
+    const formValue = this.bookForm.value;
+    this.isLoading.set(true);
+    if (!this.authService.isLoggedIn()) {
+      this.route.navigate(['/login']);
+      return;
+    }
+
+    this.bookingService
+      .bookEvent(eventId, formValue.quntity) //use the method in booking service
+      .then(() => {
+        this.route.navigate(['/bookings']);
+        this.isLoading.set(false);
+      })
+      .catch((error) => {
+        alert('Error while book the event ' + error);
+        this.isLoading.set(false);
+      });
   }
-)
-  .catch((error)=>{alert("Error while book the event "+error)
-    this.isLoading.set(false);
-  })
-}
 }
